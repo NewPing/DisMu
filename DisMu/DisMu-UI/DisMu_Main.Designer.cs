d3e54e7e4c8cf7f50c8af5aa44b7d11e5b96db9a
Florian Jo <46020908+iVlozz@users.noreply.github.com>
2020-10-22 14:16:49 +0200

    Changed Form Title

--- DisMu/DisMu-UI/DisMu_Main.Designer.cs
+++ DisMu/DisMu-UI/DisMu_Main.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // DisMu_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "DisMu_Main";
+            this.Text = "DisMu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
