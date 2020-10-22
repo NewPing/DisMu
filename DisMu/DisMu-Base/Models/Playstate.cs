ea485132d5565e7e7c0f9f28a5822b7ec34dd678
OnlyNew <jan-roth1998@web.de>
2020-10-22 13:20:00 +0200

    restructuring - created dismu-base with models

--- DisMu/DisMu-Base/Models/Playstate.cs
+++ DisMu/DisMu-Base/Models/Playstate.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace DisMu_Service.Models
+namespace DisMu_Base.Models
 {
     public static class Playstate
     {
         public static Guid currentPlaylist { get; set; }
         public static Guid currentTrack { get; set; }
-        public static DateTime startedAt { get; set;}
+        public static DateTime startedAt { get; set; }
         public static DateTime pausedAt { get; set; }
     }
 }
