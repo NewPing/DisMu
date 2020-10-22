c60deb43ab15fe912e060e6eee27507b775b9cd1
OnlyNew <jan-roth1998@web.de>
2020-10-22 13:20:00 +0200

    restructuring - created dismu-base with models

--- DisMu/DisMu-Base/Models/Track.cs
+++ DisMu/DisMu-Base/Models/Track.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace DisMu_Service.Models
+namespace DisMu_Base.Models
 {
     public class Track
     {
